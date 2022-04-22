614384eb02347e8c8859eb3329e0b346878bcdc7
Hanssel Neira <hrneiram@correo.udistrital.edu.co>
2022-04-22 14:47:20 -0500

    se añadió logica de contador

--- todo-app-cun/src/App.js
+++ todo-app-cun/src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { Component } from 'react';
+import './App.css';
+import FirstComponent from './components/learning-examples/FirstComponent';
+import SecondComponent from './components/learning-examples/SecondComponent';
+import ThirdComponent from './components/learning-examples/ThirdComponent';
+import FourthComponent from './components/learning-examples/FourthComponent';
+import Counter from './components/counter/Counter';
+ 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Counter/>
+        {/* My hello world
+        <FirstComponent></FirstComponent>
+        <SecondComponent />
+        <SecondComponent />
+        <SecondComponent />
+        <ThirdComponent />
+        <FourthComponent /> */}
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-    </div>
-  );
+        </header> */}
+      </div>
+    );
+  }
+}
+
+
+
+class LearningComponents extends Component {
+  render() {
+    return (
+      <div className="LearningComponents">
+      </div>
+    );
+  }
 }
 
 export default App;
