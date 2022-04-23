f4f8938a824baca45d9ee777c959ce91d2967e7b
Hanssel Neira <hrneiram@correo.udistrital.edu.co>
2022-04-23 02:23:19 -0500

    segundo commit todo app login

---
+++ todo-app-cun/src/components/todo/TodoApp.jsx
@@ -0,0 +1,24 @@
+import React, { Component} from 'react';
+
+class TodoApp extends Component {
+    render (){
+        return(
+            <div className="TodoApp">
+                <LoginComponent/ >
+            </div>
+        )
+    }
+}
+class LoginComponent extends Component {
+    render (){
+        return(
+            <div>
+            User Name: <input type= "text" name="username"/>
+            Password: <input type= "password" name="username"/>
+            <button>Login</button>
+            </div>
+        )
+    }
+}
+
+export default TodoApp
