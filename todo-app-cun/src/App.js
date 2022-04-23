bf6c3dca94d5bde79df7b8961b60c4f3d90ae328
Hanssel Neira <hrneiram@correo.udistrital.edu.co>
2022-04-23 02:21:10 -0500

    primer commit login feature

--- todo-app-cun/src/App.js
+++ todo-app-cun/src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import FirstComponent from './components/learning-examples/FirstComponent';
-import SecondComponent from './components/learning-examples/SecondComponent';
-import ThirdComponent from './components/learning-examples/ThirdComponent';
-import FourthComponent from './components/learning-examples/FourthComponent';
-import Counter from './components/counter/Counter';
+import TodoApp from './components/todo/TodoApp';
  
 class App extends Component {
   render() {
         return (
           <div className="App">
-            <Counter />
+            <TodoApp />
           </div>
         );
       }
--- todo-app-cun/src/components/counter/Counter.jsx
+++ todo-app-cun/src/components/counter/Counter.jsx
@@ -11,62 +11,121 @@ class Counter extends Component {
             counter : 0,
         }
         this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+        this.reset = this.reset.bind(this);
     }
 
     render() {
         return (
           <div className="counter">
-            <CounterButton by={1} incrementMethod={this.increment}/>
-            <CounterButton by={5} incrementMethod={this.increment}/>
-            <CounterButton by={10} incrementMethod={this.increment}/>
-            <span className="count">{this.state.counter}</span>
+            <CounterButton by={1} incrementMethod={this.increment} decrementMethod={this.decrement} />
+            <CounterButton by={5} incrementMethod={this.increment} decrementMethod={this.decrement} />
+            <CounterButton by={10} incrementMethod={this.increment} decrementMethod={this.decrement} />
+            <span className="count">{this.state.counter}</span> 
+            <div><button className='reset' onClick={this.reset}>RESET</button></div>
           </div>
         );
       }
       increment (by){
-          console.log("increment from parent")
-         this.setState({
-             counter : this.state.counter + by
-         });
-    }
+         this.setState(
+            (PrevState) => {
+            return{counter : PrevState.counter + by} 
+            }
+         );
+        }
+      decrement (by){
+         this.setState(
+          (PrevState) => {
+           return{counter : PrevState.counter - by} 
+            }
+        );
+     }
+
+       reset (){
+         this.setState(
+          (PrevState) => {
+            return{counter : 0} 
+            }
+         );
+        }
 }
 class CounterButton extends Component {
 
     constructor (){
         super();
-        this.state ={
-            counter : 0,
-        }
-        this.increment = this.increment.bind(this);
+        // this.state ={
+        //     counter : 0,
+        // }
+        // this.increment = this.increment.bind(this);
+        // this.decrement = this.decrement.bind(this);
     }
     render = () => {
         return (
             <div className='counter'>
-                <button onClick={this.increment}>+{this.props.by}</button>
+                <button onClick={() =>this.props.incrementMethod(this.props.by)}>+{this.props.by}</button>
+                <button onClick={() =>this.props.decrementMethod(this.props.by)}>-{this.props.by}</button>
             </div>
         )
     }
-    increment (){
-        console.log("increment from children")
-        this.setState({
-            counter : this.state.counter + this.props.by
-        });
-        this.props.incrementMethod(this.props.by)
-    }
+    // increment (){
+        
+    //     this.setState(
+    //     (PrevState) => {
+    //         return {counter : PrevState.counter + this.props.by}
+    //     }
+        
+    //     );
+    //     this.props.incrementMethod(this.props.by)
+    // }
 
     
     // decrement (){
-    //     //console.log('increment');
-    //     //this.state.counter++; Bad Practise
-    //     this.setState({
-    //         counter : this.state.counter - 1
-    //     });
+
+    //     this.setState(
+    //     (PrevState) =>{
+    //         return {counter : PrevState.counter - this.props.by}
+    //     }
+    //     );
+    //     this.props.decrementMethod(this.props.by)
+
     // }
 }
+// class ResetButton extends Component {
+//     constructor (){ 
+//         super();
+//         this.state ={
+//             counter : 0,
+//         }
+//         this.reset = this.reset.bind(this);
+//     }
+//     render = () => {
+//         return (
+//             <div className='counter'>
+//                 <button onClick={this.reset}>RESET</button>
+//             </div>
+//         )
+//     }
+//     reset (){
+        
+//         this.setState(
+//         (PrevState) => {
+//             return {counter : PrevState.counter * 0}
+//         }
+        
+//         );
+//         this.props.resetMethod(0)
+//     }
+// }
 CounterButton.defaultProps = {
     by : 1,
 }
 CounterButton.propTypes = {
    by : PropTypes.number
 }
+// ResetButton.defaultProps = {
+//     by : 0,
+// }
+// ResetButton.propTypes = {
+//     by : PropTypes.number
+//  }
 export default Counter;
